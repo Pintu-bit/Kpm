3ad93c500f9feb05c34f0c63b80137e82b327a40
Shubhadeep Mahato <shubhadeepmahato123@gmail.com>
2024-01-02 14:04:35 +0530

    little changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <footer className="bg-gray-200 p-2">
       <div className="container mx-auto flex flex-col items-center justify-center">
         <p className="text-lg text-green-900 font-bold">
-          kpmseeds.com
+          kpmseeds.in
         </p>
         <p className="text-sm text-green-700 font-medium">
           © {year} All rights reserved.
